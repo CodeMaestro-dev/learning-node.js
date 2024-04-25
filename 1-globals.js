e9eb6b663944281a385794ee85f1c43ab150f631
Joseph Nehikhare <87471100+jayjay117@users.noreply.github.com>
2024-04-25 16:33:30 +0100

    Update 1-globals.js

--- 1-globals.js
+++ 1-globals.js
@@ -1,7 +1,7 @@
 // GLOBALS - NO WINDOW
 
 // __dirname - path to current directory
-// __fiename - file name
+// __filename - file name
 // require - function to use modules (CommonJS)
 // module - info about current module (file)
 // process - info about env where the program is being executed
